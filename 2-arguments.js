c29d21a2f1d12c4edf8885a299f3586239f949bb
MaryAnn <mwihakiann902@gmail.com>
2025-09-14 21:18:07 +0300

    added js file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
-if (arguments == 0){
-    console.log("No arguments") 
-};
-
-if (arguments==1){
-    console.log("Argument found")
-}else{
-    console.log("Arguments found")
-};
+if (args === 0) {
+  console.log('No argument');
+} else {
+  if (args === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+}
