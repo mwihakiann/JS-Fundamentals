9d706fe3955b47cca8c6f07d27f8b1ab637a9302
MaryAnn <mwihakiann902@gmail.com>
2025-09-14 21:44:41 +0300

    file added

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -4,10 +4,8 @@ const second = process.argv[3];
 
 if (first === undefined) {
   console.log('No argument');
+} else if (second === undefined) {
+  console.log('1 argument: ' + first);
 } else {
-  if (second === undefined) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+  console.log('2 arguments: ' + first + ' ' + second);
 }
